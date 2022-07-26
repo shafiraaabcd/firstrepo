ef32afde8e756ae2e372281bb12867de53bf2cce
mrissa <marissarissarissa19@gmail.com>
2022-07-26 14:43:17 +0700

    first commit

---
+++ src/perulangan1/Perulangan1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perulangan1;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Perulangan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int aisyah = 1; aisyah<7; aisyah++){
+            System.out.println("a");
+        }
+    }
+    
+}
